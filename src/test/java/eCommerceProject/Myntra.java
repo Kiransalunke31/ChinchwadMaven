565397f6313783a6fa60d15f064fdfa47be916a4
ishikasalunke30 <kiransalunke0301@gmail.com>
2023-07-30 10:27:23 +0700

    [ishikasalunke30] added new Meshoo and Myntra TC

---
+++ src/test/java/eCommerceProject/Myntra.java
@@ -0,0 +1,22 @@
+package eCommerceProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Myntra {
+  @Test
+  public void myntraTest() throws InterruptedException 
+  
+  {
+  
+	  WebDriver driver= new ChromeDriver();
+	  Reporter.log("opening myntra",false);
+	  driver.get("https://www.myntra.com/");
+	  Thread.sleep(7000);
+	  driver.close();
+  
+  
+  }
+}
